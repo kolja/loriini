ab095b128cef0948538f0060c7d3e3adf3d261ca
Kolja Wilcke <kolja@apfel.de>
2023-02-22 17:15:57 +0100

    introduce Triangle stuct

--- src/triangle.rs
+++ src/triangle.rs
@@ -4,19 +4,31 @@
 use crate::model::Area;
 use palette::{LabHue, Hue, Lch, Hsl};
 
-fn point_in_triangle(px: f64, py: f64, r: f64, angle: f64) -> bool {
-    let phi = angle;
-    let x1 = r * phi.to_radians().cos();
-    let y1 = r * phi.to_radians().sin();
-    let x2 = r * (phi + 120.0).to_radians().cos();
-    let y2 = r * (phi + 120.0).to_radians().sin();
-    let x3 = r * (phi + 240.0).to_radians().cos();
-    let y3 = r * (phi + 240.0).to_radians().sin();
+struct Triangle {
+    p1: (f64, f64),
+    p2: (f64, f64),
+    p3: (f64, f64),
+}
+
+fn triangle(r: f64, phi: f64) -> Triangle {
+    Triangle {
+        p1: (r * phi.to_radians().cos(), r * phi.to_radians().sin()),
+        p2: (r * (phi + 120.0).to_radians().cos(), r * (phi + 120.0).to_radians().sin()),
+        p3: (r * (phi + 240.0).to_radians().cos(), r * (phi + 240.0).to_radians().sin())
+    }
+}
+
+fn point_in_triangle(px: f64, py: f64, t: &Triangle) -> bool {
+
+    let (x1, y1) = t.p1;
+    let (x2, y2) = t.p2;
+    let (x3, y3) = t.p3;
 
     let denom = (y2 - y3) * (x1 - x3) + (x3 - x2) * (y1 - y3);
     let alpha = ((y2 - y3) * (px - x3) + (x3 - x2) * (py - y3)) / denom;
     let beta = ((y3 - y1) * (px - x3) + (x1 - x3) * (py - y3)) / denom;
     let gamma = 1.0 - alpha - beta;
+
     alpha >= 0.0 && beta >= 0.0 && gamma >= 0.0
 }
 
@@ -24,6 +36,8 @@ impl Area {
     pub fn triangle(&mut self) -> &mut Self {
 
         let angle = self.color.hue.to_degrees() as f64;
+        let t = &triangle( self.inner_radius, angle );
+
         for i in 0..self.height {
             for j in 0..self.width {
 
@@ -33,7 +47,7 @@ impl Area {
                 let x = (j as f64 - cols2 + 0.5) * self.factorx;
                 let y = i as f64 - rows2 + 0.5;
 
-                let within = point_in_triangle(x, y, self.inner_radius, angle);
+                let within = point_in_triangle(x, y, t);
 
                 if within {
                     self.grid[i][j] = Some(self.color)
